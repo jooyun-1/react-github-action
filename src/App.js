0f151b0d97bd9bf4044a134f86c012ed7456564b
joo6881 <joo6881@gmail.com>
2023-05-29 14:19:08 +0900

    initial

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3 data-testid="counter">{count}</h3>
+        <button
+          data-testid="minus-button"
+          onClick={() => setCount((count) => count - 1)}
+          disabled={disabled}
+        >
+          -
+        </button>
+        <button
+          data-testid="plus-button"
+          onClick={() => setCount((count) => count + 1)}
+          disabled={disabled}
+        >
+          +
+        </button>
+        <button
+          style={{ backgroundColor: "blue" }}
+          data-testid="on/off-button"
+          onClick={() => setDisabled((disabled) => !disabled)}
+        >
+          on/off
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
